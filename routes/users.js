6c7198df4383abb2a716cada4f86b9865bc0e451
dcbryant <dcbryant753@gmail.com>
2018-01-01 15:49:58 +0800

    添加模板引擎并且拆分模板组件

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ const router = express.Router()
 
 // name相当于一个占位符
 router.get('/:name',(req,res) => {
-    res.send('hello：' + req.params.name)
+    // 渲染ejs模板
+    res.render('users',{
+        name:req.params.name
+    })
 })
 
 module.exports = router
